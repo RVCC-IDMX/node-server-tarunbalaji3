28884fdca0aa9564c253504dced8d43b2ba457ee
Tarun Balaji Ravi <tarunbalaji3@gmail.com>
2022-03-18 12:24:58 -0400

    Create basic web server with HTML file

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World<h1>");
+  fs.readFile("./index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  })
 });
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
